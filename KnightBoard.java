6da21a46d595b49757ad531156d28fd18fec0c24
Michael Lin <mlin01@stuy.edu>
2019-02-13 09:52:17 -0500

    added exceptions

--- KnightBoard.java
+++ KnightBoard.java
@@ -3,12 +3,25 @@ public class KnightBoard{
   public int[][] board;
 
   public KnightBoard(int startingRows, int startingCols){
+    if(startingRows<0||startingCols<0) throw new IllegalArgumentException();
     board = new int[startingRows][startingCols];
     for(int r = 0; r < startingRows; r++){
       for(int c = 0; c < startingCols;c++){
         board[r][c]=0;
       }
     }
+  }
+
+  public boolean solve(int startingRow, int startingCol){
+    if(board[0][0]!=0) throw new IllegalStateException();
+    if(startingRow<0||startingRow>=board.length||
+       startingCol<0||startingCol>board[0].length) throw new IllegalArgumentException();
+    return solveH(startingRow,startingCol,0);
+  }
+
+  private boolean solveH(int row, int col, int level){
+    if(level>board.length*board[0].length) return true;
+    
   }
 
   public String toString(){
@@ -23,5 +36,9 @@ public class KnightBoard{
     return ans;
   }
 
-  public static void main
+  public static void main(String args[]){
+    KnightBoard b = new KnightBoard(4,4);
+    b.board[2][2]=12;
+    System.out.println(b);
+  }
 }
