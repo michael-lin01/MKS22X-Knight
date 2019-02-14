2e51277eb77a30fa01e4fc57fbe784da38b6eb15
Michael Lin <mlin01@stuy.edu>
2019-02-14 10:11:06 -0500

    fixing solve

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,6 +1,7 @@
 public class KnightBoard{
 
   public int[][] board;
+  private int[][] moves = new int[][]{{-2,-1,1,2},{-2,-1,1,2}};
 
   public KnightBoard(int startingRows, int startingCols){
     if(startingRows<0||startingCols<0) throw new IllegalArgumentException();
@@ -12,20 +13,47 @@ public class KnightBoard{
     }
   }
 
+  private boolean addKnight(int row, int col, int level){
+    if(row<0 || row>=board.length || col < 0 || col >= board[row].length || board[row][col]!=0) {
+      return false;
+    }
+    board[row][col] = level;
+    return true;
+  }
+
+  private boolean removeKnight(int row, int col){
+    if(row<0 || row>=board.length || col < 0 || col >= board[row].length || board[row][col]!=0) {
+      return false;
+    }
+    board[row][col]=0;
+    return true;
+  }
+
+
   public boolean solve(int startingRow, int startingCol){
     if(board[0][0]!=0) throw new IllegalStateException();
     if(startingRow<0||startingRow>=board.length||
        startingCol<0||startingCol>=board[0].length) throw new IllegalArgumentException();
-    return solveH(startingRow,startingCol,0);
+    return solveH(startingRow,startingCol,1);
   }
 
   private boolean solveH(int row, int col, int level){
-    if(level>=board.length*board[0].length) return true;
-    if(row<0 || row>=board.length ||
-       col < 0 || col >= board[row].length || board[row][col]!=0) return false;
-    board[row][col] = level;
-    return solveH(row-2,col-1,level+1) || solveH(row-2,col+1,level+1) || solveH(row-1,col+2,level+1) || solveH(row+1,col+2,level+1)||
-           solveH(row+2,col+1,level+1) || solveH(row+2,col-1,level+1) || solveH(row+1,col-2,level+1) || solveH(row-1,col-2,level+1);
+    if(level>board.length*board[0].length) {
+      System.out.println(this);
+      return true;
+    }
+    for(int i = 0; i < moves.length;i++){
+      for(int n = 0; n < moves.length; n++){
+        if(addKnight(row+moves[0][i],col+moves[1][n],level)){
+          if (solveH(row+moves[0][i],col+moves[1][n],level+1)){
+            return true;
+          }
+          removeKnight(row,col);
+        }
+      }
+    }
+    System.out.println(this);
+    return false;
   }
 
   public String toString(){
